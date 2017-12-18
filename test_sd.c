d3f429e4b1b0eedeb6e757ec60b3fb561c952dad
Frank Theile <ftheile@grundfos.com>
2017-12-18 12:18:21 +0100

    Add a signal handler

--- test_sd.c
+++ test_sd.c
@@ -1,7 +1,32 @@
 #include <systemd/sd-journal.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
+
+static bool cont = true;
+
+static void handler(int sig, siginfo_t* si, void* unused)
+{
+	sd_journal_print(LOG_NOTICE, "in handler()");
+	cont = false;
+}
 
 int main(int argc, char *argv[])
 {
-	sd_journal_print(LOG_NOTICE, "Hello World");
+	struct sigaction sa;
+
+	sd_journal_print(LOG_NOTICE, "initializing");
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = handler;
+	if (sigaction(SIGTERM, &sa, NULL) != -1) {
+		do {
+			sd_journal_print(LOG_NOTICE, "running...");
+			sleep(10);
+		} while (cont);
+		sd_journal_print(LOG_NOTICE, "finished");
+	} else {
+		sd_journal_print(LOG_NOTICE, "initialization failed!");
+	}
 	return 0;
 }
